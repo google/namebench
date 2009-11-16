72d601df14ff874af638f1c2cdf5d50ba3175df9
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2009-11-16 20:42:55 +0000

    Add release tool for windows

---
+++ tools/release_win.bat
@@ -0,0 +1,10 @@
+PATH=C:\python26;C:\progra~2\7-zip;%PATH%
+del /q /s dist\*.*
+python setup.py py2exe
+cd dist
+7z d library.zip jinja2\* dns\* 'graphy\*
+7z a namebench_for_Windows.zip -r *
+namebench -x -O 8.8.8.8 -t5 -o test.html
+start test.html
+
+cd ..
