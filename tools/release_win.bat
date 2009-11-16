a276dc1a1df15b744fb4e8a21ad13e43365c4e22
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2009-11-16 21:22:06 +0000

    Remove w9xopen.exe and add README.txt to Windows build

--- tools/release_win.bat
+++ tools/release_win.bat
@@ -3,6 +3,8 @@ del /q /s dist\*.*
 python setup.py py2exe
 cd dist
 7z d library.zip jinja2\* dns\* 'graphy\*
+del /s w9xpopen.exe
+copy ..\README.txt .
 7z a namebench_for_Windows.zip -r *
 namebench -x -O 8.8.8.8 -t5 -o test.html
 start test.html
