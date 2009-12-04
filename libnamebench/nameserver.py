3668ca0f6fb9102cd10834bf483275d6140ed9ce
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2009-12-04 23:06:24 +0000

    Lower max store attempts and multipliers a bit more

--- libnamebench/nameserver.py
+++ libnamebench/nameserver.py
@@ -59,7 +59,7 @@ WILDCARD_DOMAINS = ('live.com.', 'blogspot.com.', 'wordpress.com.')
 
 # How many checks to consider when calculating ns check_duration
 SHARED_CACHE_TIMEOUT_MULTIPLIER = 3
-MAX_STORE_ATTEMPTS = 4
+MAX_STORE_ATTEMPTS = 2
 TOTAL_WILDCARDS_TO_STORE = 2
 
 class NameServer(object):
