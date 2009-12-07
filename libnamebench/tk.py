7bad830488d1f97763e1516cfbcf1caef5f1e069
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2009-12-07 23:27:16 +0000

    Restructure Tk code so that all messages from children threads come in via a queue mechanism. This makes popups work again under win32

--- libnamebench/tk.py
+++ libnamebench/tk.py
@@ -21,6 +21,7 @@ import sys
 import threading
 import tkFont
 import traceback
+import Queue
 from Tkinter import *
 import tkMessageBox
 
@@ -33,63 +34,102 @@ def closedWindowHandler():
   print 'Au revoir, mes amis!'
   sys.exit(1)
 
+global_message_queue = Queue.Queue()
+global_last_message = None
+
+def AddMsg(message, master=None, **kwargs):
+  """Add a message to the global queue for output."""
+  global global_message_queue
+  global global_last_message
+
+  new_message = Message(message, **kwargs)
+  if new_message != global_last_message:
+    global_message_queue.put(new_message)
+    master.event_generate('<<msg>>', when='tail')
+    global_last_message = new_message
+
+class Message(object):
+  """Messages to be passed from to the main thread from children.
+  
+  Used to avoid thread issues inherent with Tk.
+  """
+  def __init__(self, message, error=False, count=False, total=False,
+               enable_button=None, debug=False):
+    self.message = message
+    self.error = error
+    self.count = count
+    self.debug = debug
+    self.total = total
+    self.enable_button = enable_button
+
 class WorkerThread(threading.Thread, base_ui.BaseUI):
   """Handle benchmarking and preparation in a separate UI thread."""
 
-  def __init__(self, primary, secondary, options, status_callback=None,
-               runstate_callback=None, history_parser=None):
-    self.UpdateStatus('created workerthread')
+  def __init__(self, primary, secondary, options, history_parser=None, master=None):
     threading.Thread.__init__(self)
-    self.runstate_callback = runstate_callback
-    self.status_callback = status_callback
+    self.status_callback = self.msg
     self.hparser = history_parser
     self.primary = primary
+    self.master = master
     self.secondary = secondary
     self.options = options
     self.resource_dir = os.path.dirname(os.path.dirname(__file__))
 
+  def msg(self, message, **kwargs):
+    """Add messages to the main queue."""
+    return AddMsg(message, master=self.master, **kwargs)
+  
   def run(self):
-    if self.runstate_callback:
-      self.runstate_callback(running=True)
+    self.msg('Started thread', enable_button=False)
     try:
       self.PrepareBenchmark()
       self.UpdateStatus('Here comes the pain...')
       self.RunBenchmark()
     except nameserver_list.OutgoingUdpInterception:
       (exc_type, exception, tb) = sys.exc_info()
-      self.UpdateStatus('Outgoing requests were intercepted!',
-                        error=exception)
+      self.msg('Outgoing requests were intercepted!', error=exception)
     except nameserver_list.TooFewNameservers:
       (exc_type, exception, tb) = sys.exc_info()
-      self.UpdateStatus('Too few nameservers to test', error=exception)
+      self.msg('Too few nameservers to test', error=exception)
     except:
       (exc_type, exception, tb) = sys.exc_info()
-      print "-- FAIL ----------------------------------------------------------"
       traceback.print_exc(tb)
-      print "------------------------------------------------------------------"
       error_msg = '\n'.join(traceback.format_tb(tb)[-4:])
-      self.UpdateStatus('FAIL: %s' % exception, error=error_msg)
-    if self.runstate_callback:
-      self.runstate_callback(running=False)
+      self.msg(exception, error=error_msg)
+    self.msg(None, enable_button=True)
 
 
-class NameBenchGui(Frame, base_ui.BaseUI):
-  """The main Tk GUI class."""
-
+class NameBenchGui(object):
+  """The main GUI."""
   def __init__(self, options, supplied_ns, global_ns, regional_ns, version=None):
     self.options = options
     self.supplied_ns = supplied_ns
     self.global_ns = global_ns
     self.regional_ns = regional_ns
     self.version = version
-    Frame.__init__(self)
-    self.master.protocol('WM_DELETE_WINDOW', closedWindowHandler)
 
   def Execute(self):
-    """Called by namebench.py, begins the UI drawing process."""
-    self.DrawWindow()
-    self.mainloop()
+    self.root = Tk()
+    app = MainWindow(self.root, self.options, self.supplied_ns, self.global_ns, self.regional_ns, self.version)
+    app.DrawWindow()
+    self.root.bind('<<msg>>', app.MessageHandler)
+    self.root.mainloop()
+    
 
+class MainWindow(Frame, base_ui.BaseUI):
+  """The main Tk GUI class."""
+
+  def __init__(self, master, options, supplied_ns, global_ns, regional_ns, version=None):
+    """TODO(tstromberg): Remove duplication from NameBenchGui class."""
+    Frame.__init__(self)
+    self.master = master
+    self.options = options
+    self.supplied_ns = supplied_ns
+    self.global_ns = global_ns
+    self.regional_ns = regional_ns
+    self.version = version
+    self.master.protocol('WM_DELETE_WINDOW', closedWindowHandler)
+  
   def DrawWindow(self):
     """Draws the user interface."""
     self.nameserver_form = StringVar()
@@ -174,11 +214,23 @@ class NameBenchGui(Frame, base_ui.BaseUI):
     self.UpdateRunState(running=False)
     self.UpdateStatus('Ready')
 
+  def MessageHandler(self, event):
+    """Pinged when there is a new message in our queue to handle."""
+    while global_message_queue.qsize():
+      msg = global_message_queue.get()
+      if msg.error:
+        self.ErrorPopup(msg.message, msg.error)
+      elif msg.enable_button == False:
+        self.UpdateRunState(running=True)
+      elif msg.enable_button == True:
+        self.UpdateRunState(running=False)
+      self.UpdateStatus(msg.message, count=msg.count, total=msg.total, error=msg.error)
 
-  def UpdateStatus(self, message, count=None, total=None, error=None):
+  def UpdateStatus(self, message, count=None, total=None, error=None, debug=False):
     """Update our little status window."""
+    if not message:
+      return None
 
-    # TODO(tstromberg): Add specific Error support
     if total:
       state = '%s... [%s/%s]' % (message, count, total)
     elif count:
@@ -186,12 +238,12 @@ class NameBenchGui(Frame, base_ui.BaseUI):
     else:
       state = message
 
-    if error:
-      print "> %s [%s]" % (state, error)
-      tkMessageBox.showerror(message, error)
-    else:
-      print "> %s" % state
+    print "> %s" % state
     self.status.set(state[0:60])
+
+  def ErrorPopup(self, title, message):
+    print "Showing popup: %s" % title
+    tkMessageBox.showerror(title, message, master=self.master)
 
   def UpdateRunState(self, running=True):
     # try/except blocks added to work around broken Tcl/Tk libraries
@@ -221,14 +273,12 @@ class NameBenchGui(Frame, base_ui.BaseUI):
     self.ProcessForm()
     thread = WorkerThread(self.primary, self.secondary, self.options,
                           history_parser=self.hparser,
-                          status_callback=self.UpdateStatus,
-                          runstate_callback=self.UpdateRunState)
+                          master=self.master)
     thread.start()
 
   def ProcessForm(self):
     """Read form and populate instance variables."""
     self.primary = self.supplied_ns + util.ExtractIPTuplesFromString(self.nameserver_form.get())
-    print 'GLOBAL: %s' % self.use_global.get()
 
     if self.use_global.get():
       self.primary += self.global_ns
