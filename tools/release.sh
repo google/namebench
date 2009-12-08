40976f3549073d565b1ddb86e08bbbfa12c05f27
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2009-12-08 20:59:40 +0000

    Add a ChangeLog.txt file

--- tools/release.sh
+++ tools/release.sh
@@ -22,6 +22,7 @@ version=`grep '^VERSION' namebench-$$/namebench.py | cut -d\' -f2`
 mv namebench-$$ namebench-$version
 cd namebench-$version
 ./namebench.py -t2 -r1 -j40 -o /tmp/$$.csv 208.67.220.220
+svn log > ChangeLog.txt
 find . -name "*.pyc" -delete
 find . -name ".svn" -exec rm -Rf {} \; 2>/dev/null
 cd ..
