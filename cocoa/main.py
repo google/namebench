583e363531255e793b45f267e457f9d84757a0b9
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2009-12-09 23:49:09 +0000

    Explicitly declare the Apache 2.0 license for autogenerated PyObjC files

--- cocoa/main.py
+++ cocoa/main.py
@@ -1,10 +1,20 @@
+# Copyright 2009 Google Inc. All Rights Reserved.
 #
-#  main.py
-#  namebench
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#  Created by Thomas Stromberg on 9/10/09.
-#  Copyright __MyCompanyName__ 2009. All rights reserved.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Cocoa frontend loader for namebench."""
+
+__author__ = 'tstromberg@google.com (Thomas Stromberg)'
 
 #import modules required by application
 import objc
