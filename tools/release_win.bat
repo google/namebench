2cd9ef34e6cd0864aac1ac615695fffaab2a0a1f
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2009-12-10 22:25:52 +0000

    We no longer need to manipulate library.zip due to recent setup.py changes

--- tools/release_win.bat
+++ tools/release_win.bat
@@ -2,8 +2,7 @@ PATH=C:\python26;C:\progra~1\7-zip;C:\progra~2\7-zip;%PATH%
 del /q /s dist\*.*
 python setup.py py2exe
 cd dist
-7z d library.zip jinja2\* dns\* 'graphy\*
-del /s w9xpopen.exe
+#7z d namebench.zip jinja2\* dns\* 'graphy\*
 copy ..\README.txt .
 7z a namebench_for_Windows.zip -r *
 namebench -x -O 8.8.8.8 -t5 -o test.html
