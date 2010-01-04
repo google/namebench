4141e566e61a521a7b1a0b39975b8b40c1a0d0ea
thomas.r.stromberg <thomas.r.stromberg@3e680942-6fdc-11de-8b73-c1edde54a3c7>
2010-01-04 22:16:26 +0000

    Fix GZIP variable to -9

--- tools/release_src.sh
+++ tools/release_src.sh
@@ -26,5 +26,5 @@ svn log > ChangeLog.txt
 find . -name "*.pyc" -delete
 find . -name ".svn" -exec rm -Rf {} \; 2>/dev/null
 cd ..
-GZIP="gzip -9" tar -zcvf namebench-${version}.tgz namebench-${version}/
+GZIP="-9" tar -zcvf namebench-${version}.tgz namebench-${version}/
 rm -Rf namebench-${version}
