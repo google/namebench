aa3029f962e47a6c0cd3023192616acb8eff5b75
Thomas Stromberg <tstromberg@google.com>
2010-05-07 18:43:24 +0000

    Insert third_party into the second slot of sys.path rather than the last slot

--- third_party/__init__.py
+++ third_party/__init__.py
@@ -2,4 +2,4 @@ import os.path
 import sys
 
 # This bit of evil should inject third_party into the path for relative imports.
-sys.path.append(os.path.dirname(__file__))
+sys.path.insert(1, os.path.dirname(__file__))
