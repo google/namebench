92027c7cf34ad2cb4f4a12c1da617a1a996b7188
Thomas Stromberg <tstromberg@google.com>
2010-07-14 05:49:20 +0000

    Remove servers.cfg

--- tools/check_nameserver_popularity.py
+++ tools/check_nameserver_popularity.py
@@ -12,10 +12,10 @@ APP_ID = 'P5ihFKzV34G69QolFfb3nN7p0rSsYfC9tPGq.IUS.NLWEeJ14SG9Lei0rwFtgwL8cDBrA6
 QUERY_MODIFIERS = '-site:txdns.net -site:sitedossier.com -mx -site:dataopedia.com -site:l0t3k.net -syslog -"4.2.2.1" -site:cqcounter.com -site:flow.nttu.edu.tw -site:websiteoutlook.com -site:ipgeolocator.com -site:tdyndns.org -site:ebrara.com -site:onsamehost.com -site:ipaddresscentral.com -site:quia.jp -inetnum -site:domaintools.com -site:domainbyip.com -site:pdos.csail.mit.edu  -statistics  -"country name" -"Q_RTT" -site:botsvsbrowsers.com -"ptr record" -site:ip-db.com -site:chaip.com.cn -site:lookup365.com -"IP Country" -site:iptoolboxes.com -"Unknown Country" -"Q_RTT" -amerika -whois -Mozilla -site:domaincrawler.com -site:geek-tools.org -site:visualware.com -site:robtex.com -site:domaintool.se -site:opendns.se -site:ungefiltert-surfen.de -site:datakitteh.org -"SLOVAKIA (SK)" -"IP Search" -site:www.medicore.com.ua -site:dig.similarbase.com -site:ipcorporationwiki.com -site:coolwhois.com -site:corporationwiki.com -site:iptool.us'
 CACHE_DIR = os.getenv('HOME') + '/.ycache'
 BANNED_URL_KEYWORDS = [
-    '\.xls$', '\.txt$', 'spam', 'nettools', 'namebench', 'spam', 'affinity-v1',
+    '\.xls$', '\.txt$', 'spam', 'nettools', 'namebench\.cfg', 'spam', 'affinity-v1',
     'corporationwiki', 'iptools', 'whois', 'iana.org', 'public.*dns', 'blocked',
     'firewall', 'websitevaluespy', 'iptool', 'sshd-versions', '\.off$',
-    'dnsstuff', 'secspider'
+    'dnsstuff', 'secspider', 'servers\.cfg'
 ]
 
 def CheckPopularity(ip):
