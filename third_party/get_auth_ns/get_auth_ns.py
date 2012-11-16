9370653a2d7cbae6f17edccfce090beec473aca2
Thomas Stromberg <tstromberg@google.com>
2012-11-15 22:19:53 -0800

    Add get_auth_ns

---
+++ third_party/get_auth_ns/get_auth_ns.py
@@ -0,0 +1,47 @@
+import dns
+import dns.name
+import dns.query
+import dns.resolver
+
+def get_authoritative_nameserver(domain, log=lambda msg: None):
+    n = dns.name.from_text(domain)
+
+    depth = 2
+    default = dns.resolver.get_default_resolver()
+    nameserver = default.nameservers[0]
+
+    last = False
+    while not last:
+        s = n.split(depth)
+
+        last = s[0].to_unicode() == u'@'
+        sub = s[1]
+
+        log('Looking up %s on %s' % (sub, nameserver))
+        query = dns.message.make_query(sub, dns.rdatatype.NS)
+        response = dns.query.udp(query, nameserver)
+
+        rcode = response.rcode()
+        if rcode != dns.rcode.NOERROR:
+            if rcode == dns.rcode.NXDOMAIN:
+                raise Exception('%s does not exist.' % sub)
+            else:
+                raise Exception('Error %s' % dns.rcode.to_text(rcode))
+
+        rrset = None
+        if len(response.authority) > 0:
+            rrset = response.authority[0]
+        else:
+            rrset = response.answer[0]
+
+        rr = rrset[0]
+        if rr.rdtype == dns.rdatatype.SOA:
+            log('Same server is authoritative for %s' % sub)
+        else:
+            authority = rr.target
+            log('%s is authoritative for %s' % (authority, sub))
+            nameserver = default.query(authority).rrset[0].to_text()
+
+        depth += 1
+
+    return nameserver
