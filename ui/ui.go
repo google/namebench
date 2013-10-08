2c4d441efbadc006d0e8b3d11a613c6e68d2916f
Thomas Stromberg <tstromberg@google.com>
2013-10-08 16:00:14 -0700

    Wire up index.html template.

--- ui/ui.go
+++ ui/ui.go
@@ -2,11 +2,13 @@
 package ui
 
 import (
-	"fmt"
-	"github.com/google/namebench/dnsqueue"
-	"github.com/google/namebench/history"
+	"html/template"
 	"log"
 	"net/http"
+	"strings"
+
+	"github.com/google/namebench/dnsqueue"
+	"github.com/google/namebench/history"
 )
 
 const (
@@ -15,44 +17,65 @@ const (
 
 	// Number of workers (same as Chrome's DNS prefetch queue)
 	WORKERS = 8
+
+	// Number of tests to run
+	COUNT = 50
+
+	// How far back to reach into browser history
+	HISTORY_DAYS = 30
 )
 
+var (
+	indexTmpl = loadTemplate("ui/index.html")
+)
+
+// RegisterHandler registers all known handlers.
 func RegisterHandlers() {
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/submit", Submit)
 }
 
-// Index handles /
-func Index(w http.ResponseWriter, r *http.Request) {
-	records, err := history.Chrome(30)
+// loadTemplate loads a set of templates.
+func loadTemplate(paths ...string) *template.Template {
+	t := template.New(strings.Join(paths, ","))
+	_, err := t.ParseFiles(paths...)
 	if err != nil {
 		panic(err)
 	}
+	return t
+}
 
-	q := dnsqueue.StartQueue(QUEUE_LENGTH, WORKERS)
-	hostnames := history.Random(16, history.Uniq(history.ExternalHostnames(records)))
-
-	for _, record := range hostnames {
-		q.Add("8.8.8.8:53", "A", record+".")
-		log.Printf("Added %s", record)
-	}
-	log.Printf("Sending comp")
-	q.SendCompletionSignal()
-	log.Printf("comp sent")
-	answered := 0
-
-	for {
-		if answered == len(hostnames) {
-			break
-		}
-		result := <-q.Results
-		answered += 1
-		fmt.Fprintf(w, "%s of %s responses complete: %s", answered, len(hostnames), result)
+// Index handles /
+func Index(w http.ResponseWriter, r *http.Request) {
+	if err := indexTmpl.ExecuteTemplate(w, "index.html", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	return
 }
 
 // Submit handles /submit
 func Submit(w http.ResponseWriter, r *http.Request) {
+	records, err := history.Chrome(HISTORY_DAYS)
+	if err != nil {
+		panic(err)
+	}
+
+	q := dnsqueue.StartQueue(QUEUE_LENGTH, WORKERS)
+	hostnames := history.Random(COUNT, history.Uniq(history.ExternalHostnames(records)))
+
+	for _, record := range hostnames {
+		q.Add("8.8.8.8:53", "A", record+".")
+		log.Printf("Added %s", record)
+	}
+	q.SendCompletionSignal()
+	answered := 0
+	for {
+		if answered == len(hostnames) {
+			break
+		}
+		result := <-q.Results
+		answered += 1
+		log.Printf("%s", result)
+	}
 	return
 }
