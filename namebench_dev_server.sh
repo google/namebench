39cd8f2652a9d73eee572d33d78e1d1a1e96a060
Thomas Stromberg <tstromberg@google.com>
2013-10-10 16:37:38 -0700

    Fix bootstrap CSS path, make dev server kill namebench process at startup.

--- namebench_dev_server.sh
+++ namebench_dev_server.sh
@@ -2,8 +2,8 @@
 # poor mans multi-platform restart for the dev server.
 # requires: watchdog 0.6.0
 
+killall namebench
 ./build.sh && go run namebench.go --port 9080 &
-
 
 watchmedo shell-command \
   --patterns="*.go;*.html;*.css;*.tmpl" \
